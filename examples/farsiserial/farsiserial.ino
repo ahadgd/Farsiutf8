01b9f4f7b6e6e6fc7173358a4a1a71c493356e1c
ahadgd <ahad.gd@gmail.com>
2017-01-20 13:37:28 +0330

    Update farsiserial.ino

--- examples/farsiserial/farsiserial.ino
+++ examples/farsiserial/farsiserial.ino
@@ -5,11 +5,11 @@ Farsiutf8 utf8;
 void setup() {
   Serial.begin(9600);
   Serial.println();
-  Serial.println(utf8.utf8ascii("بنام خدا"));
-  Serial.println(utf8.utf8ascii("کتابخانه utf8 فارسی"));
-  Serial.println(utf8.utf8ascii("این کتابخانه اولین ورژن بوده و وجود اشکال در آن محتمل می باشد"));
-  Serial.println(utf8.utf8ascii("ا ب پ ت ث ج چ ح خ د ذ ر ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی"));
-  Serial.println(utf8.utf8ascii("آبپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی"));
+  Serial.println(utf8.utf8("بنام خدا"));
+  Serial.println(utf8.utf8("کتابخانه utf8 فارسی"));
+  Serial.println(utf8.utf8("این کتابخانه اولین ورژن بوده و وجود اشکال در آن محتمل می باشد"));
+  Serial.println(utf8.utf8("ا ب پ ت ث ج چ ح خ د ذ ر ز ژ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن و ه ی"));
+  Serial.println(utf8.utf8("آبپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی"));
 }
 
 void loop() {
